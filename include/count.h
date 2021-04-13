8f600f750435a66878c30c3ce7fb469beaefd3a1
bartem1us <bartem1us.net@gmail.com>
2021-04-13 03:53:11 -0700

    count changed

---
+++ include/count.h
@@ -0,0 +1,12 @@
+//
+// Created by bartem1us on 4/13/21.
+//
+#inlcude <iostream>
+#include <string>
+
+#ifndef RK1_COUNT_H
+#define RK1_COUNT_H
+
+size_t count(const std::string &str1);
+
+#endif //RK1_COUNT_H
--- sources/count.cpp
+++ sources/count.cpp
@@ -1,2 +1,6 @@
+#include "count.h"
 
+size_t count(const std::string &str1) {
+ return str1.size();
+}
 
